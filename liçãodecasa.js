aedb08d03a801625bc9bb49be06e50b123e38cf4
marcelopl82 <marceo.8468@gmail.com>
2024-05-29 19:47:52 -0300

    addeeventerlistera . aula js

--- liçãodecasa.js
+++
@@ -1,9 +0,0 @@
-function  botãoclicado() {
-
-
-     const input= (input.value)}
-     function minhacasa(){ console.log("minhacasa")
-
-     const variavel = "minha casa"
-     console.log("variavel")}
-     const element=document.querySelector("minhacasa")
