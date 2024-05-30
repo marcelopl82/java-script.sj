f6e2cc5d866ca9330990e8c8445288b018ec0788
marcelopl82 <marceo.8468@gmail.com>
2024-05-30 19:10:49 -0300

    addevent aula

---
+++ addeventlistener.js
@@ -0,0 +1,11 @@
+
+const input = document.querySelector("#main-input")
+const select = document.querySelector("select")
+const button = document.querySelector(".main-button")
+
+select.addEventListener( "change", function troqueiovalor(abacate){
+  /*  console.log  ("Troquei de valor")*/console.log(abacate)
+})
+
+
+
