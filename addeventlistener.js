93bd912b57c4741d2a28694e10e8a2e0f5b695ff
marcelopl82 <marceo.8468@gmail.com>
2024-05-31 19:22:44 -0300

    money converter prog.1

--- addeventlistener.js
+++ addeventlistener.js
@@ -3,9 +3,9 @@ const input = document.querySelector("#main-input")
 const select = document.querySelector("select")
 const button = document.querySelector(".main-button")
 
-select.addEventListener( "change", function troqueiovalor(abacate){
-  /*  console.log  ("Troquei de valor")*/console.log(abacate)
-})
+ function troqueiovalor(abacate){
+  /*  console.log  ("Troquei de valor")*/console.log(abacate)}
 
+select.addEventListener( "change", troqueiovalor)
 
 
