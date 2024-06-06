16fc40c382d57ef0007794e0b7faf205b91d1ab6
marcelopl82 <marceo.8468@gmail.com>
2024-06-06 20:28:16 -0300

    maneyconver aula 5

---
+++ assets/convertermoney.js
@@ -0,0 +1,7 @@
+const converterbutton = document.querySelector(".converter-button")
+function convertervalues()
+{const inputcurrencyvalue=document.querySelector(".input-currency").value
+   
+console.log(inputcurrencyvalue)}
+ converterbutton.addEventListener("click", convertervalues)
+    
