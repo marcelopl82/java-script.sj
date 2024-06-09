c39e9d4ef07e905932a604df6803d93f118db572
marcelopl82 <marceo.8468@gmail.com>
2024-06-09 20:25:32 -0300

    converter money aula 6

--- assets/convertermoney.js
+++ assets/convertermoney.js
@@ -1,7 +1,21 @@
 const converterbutton = document.querySelector(".converter-button")
 function convertervalues()
 {const inputcurrencyvalue=document.querySelector(".input-currency").value
+
+   const currencyvaluetoconvert=document.querySelector(".currency-value-to-convert") //valor real
+   const currencyvaluetoconverted=document.querySelector(".currency-value")//outras moedas
    
-console.log(inputcurrencyvalue)}
+   const dolartoday= 5.20
+
+   const convertervalues=inputcurrencyvalue/dolartoday
+
+ currencyvaluetoconvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+   stile: "currency",
+   currency:"BRL"
+ }).format(inputcurrencyvalue)
+
+ currencyvaluetoconverted.innerHTML=convertervalues
+}
+
  converterbutton.addEventListener("click", convertervalues)
     
